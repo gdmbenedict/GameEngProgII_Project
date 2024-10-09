a9796ae5d547ed971932f907585bde64e5503add
Matthieu Benedict <97464794+gdmbenedict@users.noreply.github.com>
2024-10-09 17:56:51 -0300

    New Input System
    Reworked input script to use Unity's new Input System.

--- Assets/Scripts/Managers/InputManager.cs
+++ Assets/Scripts/Managers/InputManager.cs
@@ -1,6 +1,9 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Animations;
+using UnityEngine.InputSystem;
+using static UnityEngine.InputSystem.InputAction;
 
 // Sam Robichaud 
 // NSCC Truro 2024
@@ -11,7 +14,6 @@ public class InputManager : MonoBehaviour
     // Script References
     [SerializeField] private PlayerLocomotionHandler playerLocomotionHandler;
     [SerializeField] private CameraManager cameraManager; // Reference to CameraManager
-
 
     [Header("Movement Inputs")]
     public float verticalInput;
@@ -25,6 +27,7 @@ public class InputManager : MonoBehaviour
     public Vector2 cameraInput; // Mouse input for the camera
 
     public bool isPauseKeyPressed = false;
+    public bool isSprinting = false;
 
 
     public void HandleAllInputs()
@@ -36,22 +39,64 @@ public class InputManager : MonoBehaviour
         HandlePauseKeyInput();
     }
 
+    public void GetMoveInput(InputAction.CallbackContext context)
+    {
+        movementInput = context.ReadValue<Vector2>();
+    }
+
+    public void GetJumpInput(InputAction.CallbackContext context)
+    {
+        jumpInput = context.performed;
+    }
+
+    public void GetPauseInput(InputAction.CallbackContext context)
+    {
+        //way pause is handled is not working well
+
+        /*
+        if (context.performed)
+        {
+            isPauseKeyPressed = true;
+        }
+        else
+        {
+            isPauseKeyPressed = false;
+        }
+        */
+    }
+
+    public void GetSprintInput(InputAction.CallbackContext context)
+    {
+        isSprinting = context.action.IsPressed();
+    }
+
+    public void GetZoom(InputAction.CallbackContext context)
+    {
+        scrollInput = context.ReadValue<float>();
+    }
+
+    public void GetLook(InputAction.CallbackContext context)
+    {
+        cameraInput = context.ReadValue<Vector2>();
+    }
+
     private void HandleCameraInput()
     {        
             // Get mouse input for the camera
-            cameraInput = new Vector2(Input.GetAxis("Mouse X"), Input.GetAxis("Mouse Y"));
+            //cameraInput = new Vector2(Input.GetAxis("Mouse X"), Input.GetAxis("Mouse Y"));
 
             // Get scroll input for camera zoom
-            scrollInput = Input.GetAxis("Mouse ScrollWheel");
+            //scrollInput = Input.GetAxis("Mouse ScrollWheel");
 
             // Send inputs to CameraManager
             cameraManager.zoomInput = scrollInput;
             cameraManager.cameraInput = cameraInput;        
     }
 
+    
     private void HandleMovementInput()
     {
-        movementInput = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+        //movementInput = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
         horizontalInput = movementInput.x;
         verticalInput = movementInput.y;
         moveAmount = Mathf.Clamp01(Mathf.Abs(horizontalInput) + Mathf.Abs(verticalInput));
@@ -64,7 +109,7 @@ public class InputManager : MonoBehaviour
 
     private void HandleSprintingInput()
     {
-        if (Input.GetKey(KeyCode.LeftShift) && moveAmount > 0.5f)
+        if (isSprinting && moveAmount > 0.5f)
         {
             playerLocomotionHandler.isSprinting = true;
         }
@@ -76,7 +121,7 @@ public class InputManager : MonoBehaviour
 
     private void HandleJumpInput()
     {
-        jumpInput = Input.GetKeyDown(KeyCode.Space); // Detect jump input (spacebar)
+        //jumpInput = Input.GetKeyDown(KeyCode.Space); // Detect jump input (spacebar)
         if (jumpInput)
         {
             playerLocomotionHandler.HandleJump(); // Trigger jump in locomotion handler
